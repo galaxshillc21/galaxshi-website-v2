62b1b2e923c516a895013fc69b65ebdf60b14c07
ArcherDev93 <perezalfredo14@gmail.com>
2024-05-30 15:44:45 +0100

    Updating text and font size in home page

--- app/page.tsx
+++ app/page.tsx
@@ -11,20 +11,22 @@ export default function Home() {
 					baseHue={200}
 					className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
 				>
-					<h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+					<h1 className="text-white text-2xl md:text-6xl font-bold text-center">
 						GALAXSHI LLC
-					</h2>
-					<p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-						We are a married couple of developers, passionate about creating
-						funcitonal websites.
+					</h1>
+					<p className="text-white text-center md:text-2xl max-w-xl mt-6">
+						We are a dynamic duo of married developers, passionate about
+						crafting functional and beautiful websites.
 					</p>
 					<div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
 						{/* <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
 							Order now
 						</button> */}
 						<div className="px-4 py-2  text-white text-center">
-							<p>Our site is currently in development.</p>
-							<p>
+							<p className="text-sm  max-w-xl text-center">
+								Our site is currently in development.
+							</p>
+							<p className="text-sm  max-w-xl text-center">
 								Please contact us at{" "}
 								<a href="mailto:galaxshi.llc@gmail.com">
 									<u>galaxshi.llc@gmail.com</u>
